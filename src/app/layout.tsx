eec89c6a79b2c9e97adc6baba5fc05bce54286d2
Lucas Rodrigues da Silva <lrdspc@gmail.com>
2025-08-31 23:43:28 +0000

    Crie um aplicativo completo e totalmente funcional para pesquisas eleito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/icon-192x192.png" />
       </head>
       <body className={cn("font-body antialiased", fontBody.variable, fontHeadline.variable)}>
         {children}
