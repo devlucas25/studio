9c66116faa458b0ed1789b91f0afd99ea1c2ea41
Lucas Rodrigues da Silva <lrdspc@gmail.com>
2025-09-01 00:02:01 +0000

    o que falta

---
+++ src/middleware.ts
@@ -0,0 +1,83 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  const { pathname } = request.nextUrl
+
+  if (!session && (pathname.startsWith('/admin') || pathname.startsWith('/interviewer'))) {
+    const loginUrl = pathname.startsWith('/admin') ? '/login/admin' : '/login/interviewer';
+    return NextResponse.redirect(new URL(loginUrl, request.url))
+  }
+
+
+  if (session && (pathname.startsWith('/login'))) {
+     return NextResponse.redirect(new URL('/admin/dashboard', request.url))
+  }
+
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    '/admin/:path*',
+    '/interviewer/:path*',
+    '/login/:path*',
+  ],
+}
